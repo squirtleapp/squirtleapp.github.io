8b602c2ff2fdcb8b6993b714f7f19c6ff5722ad8
squirtleapp <101915082+squirtleapp@users.noreply.github.com>
2022-03-19 16:17:02 +1030

    testing

--- Squirtle.js
+++ Squirtle.js
@@ -1,8 +1,8 @@
 //Initialisation
 
 
-
-var overallDict = readFileSync("./wordlist.txt").toString('utf-8');
+var ovarall = FileReader.readAsText('wordlist.txt');
+//var overallDict = readFileSync("./wordlist.txt").toString('utf-8');
 var answerBank = ['water', 'juice', 'urine', 'honey', 'blood', 'fluid', 'sauce', 'booze', 'cider', 'broth', 'bevvy', 'lager', 'lassi', 'latte', 'pimms', 'punch', 'stout', 'tonic', 'vodka', 'hooch', 'juicy', 'drink', 'turps', 'pepsi', 'fanta'];
 
 print(overallDict);
