a41dacb7a2da0565e04b2d72e7968955524b8d57
squirtleapp <101915082+squirtleapp@users.noreply.github.com>
2022-03-21 18:57:35 +1030

    Delete Squirtle.js

--- Squirtle.js
+++
@@ -1,8 +0,0 @@
-//Initialisation
-
-var textBank = document.getElementById("wordlist.txt").files[0];
-var ovarall = FileReader.readAsText(textBank);
-//var overallDict = readFileSync("./wordlist.txt").toString('utf-8');
-var answerBank = ['water', 'juice', 'urine', 'honey', 'blood', 'fluid', 'sauce', 'booze', 'cider', 'broth', 'bevvy', 'lager', 'lassi', 'latte', 'pimms', 'punch', 'stout', 'tonic', 'vodka', 'hooch', 'juicy', 'drink', 'turps', 'pepsi', 'fanta'];
-
-print(overallDict);
